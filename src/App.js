dbb7e1b023ecd6fc58943d0a38b32fcb2402787c
unknown <2550756431@qq.com>
2020-11-30 16:37:33 +0800

    新建项目

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Page1 from './pages/index'
+
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Learn React-DnD
+      </header>
+      {/* 引入文件子组件 */}
+      <Page1></Page1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Page1() {
+    return (
+        <>
+            page1
+        </>
+    )
+}
+
+export default Page1;
