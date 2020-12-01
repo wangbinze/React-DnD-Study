e5f94fc705be31bdd5cbd40235b219635d2cdf1f
unknown <2550756431@qq.com>
2020-12-01 18:05:37 +0800

    xxx

--- react-dnd-study/src/pages/demo2/Knight.jsx
+++ react-dnd-study/src/pages/demo2/Knight.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
-
+import tsIcon from "./123.jpg"
 export default function Knight() {
-    return <span>♘</span>
+    return (
+        <div>
+            <span>♘</span>
+            <img src={tsIcon} />
+        </div>
+    )
 }
